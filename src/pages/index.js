2846725d56c31772ef26a720f8f6c747af2ee9eb
Xane <halo.milton@gmail.com>
2022-03-27 20:58:58 -0400

    "Finished Gatsby Tutorial Part 3"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from './layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Kingdom Hearts 2 poster"
+        src="../images/KH2-cover.webp"
+      />
     </Layout>
   )
 }
